4fac6bba0eb0c1f63e115de78079aa19229707d6
DannyPhang <dannyphang64@gmail.com>
2024-05-06 15:43:07 +0800

    config

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-import { createServer } from "http";
-import app from "./app.js";
+import { WebSocketServer } from "ws";
+import http from "http";
+import express from "express";
 
-const port = process.env.PORT || 1002;
+const port = 6969;
+const server = http.createServer(express);
+const wss = new WebSocketServer({ server });
 
-const server = createServer(app);
-console.log(`server is running at port: ${port}...`);
-// const wss = new WebSocket.Server({ server });
+wss.on("connection", (ws) => {
+  ws.on("message", function incoming() {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
